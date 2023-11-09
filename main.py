972f57995f2f9cf4e5f8291eea882498115e3add
CoboAr <cobo.arnold@gmail.com>
2023-11-09 18:13:48 -0500

    Quiz Application Complete

---
+++ main.py
@@ -0,0 +1,16 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+from ui import QuizInterface
+
+# Create an empty list to store the questions retrieved from API
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+# Create Quiz Graphic Interface and start the game
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
